5f212ad1378863b253e91941a4c3631d5f2980a2
Vusal Dev <vusaldev@gmail.com>
2025-03-30 17:24:41 +0400

    feature: Add dotnet projects

---
+++ languages/dotnet/Common/Application.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using MySqlConnector;
+
+namespace Application;
+
+public static class LocalStorage
+{
+    public static int Amount { get; set; } = 0;
+}
+
+public record TestData
+{
+    public string name { get; set; }
+
+    public int code { get; set; }
+}
+
+public class MySqlStorage(MySqlConnection connection)
+{
+    private const string TargetRecordName = "amount";
+
+    public async Task<TestData?> GetData()
+    {
+        var results = await connection.QueryAsync<TestData>("SELECT * FROM test WHERE name = @name LIMIT 1", new { name = TargetRecordName });
+        return results.FirstOrDefault();
+    }
+
+    public async Task<bool> UpdateData(decimal code)
+    {
+        return await connection.ExecuteAsync("UPDATE test SET code = @code WHERE name = @name", new { code, name = TargetRecordName }) > 0;
+    }
+}
