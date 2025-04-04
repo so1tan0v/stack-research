6b70b5e781cdcf54c821c944ae999ca4a27afec9
soltanoff <ilia@soltanov.dev>
2025-04-04 13:20:27 +0300

    feature: Granian app speed-up refactoring

--- languages/python/granian/main.py
+++ languages/python/granian/main.py
@@ -1,97 +1,143 @@
+import asyncio
+import logging
 import os
-import asyncmy
-import json
+import sys
 
+import asyncmy
+import orjson
 from dotenv import load_dotenv
+from granian import Granian
+from granian.constants import Interfaces, Loops
+from granian.rsgi import HTTPProtocol, Scope
+
 
 dotenv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../env/.env")
 
 load_dotenv(dotenv_path=dotenv_path)
 
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_SCHEMA = os.getenv("MYSQL_SCHEMA")
-PORT = int(os.getenv("PYTHON_PORT"))
+MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_USER = os.getenv("MYSQL_USER", "testuser")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "testpassword")
+MYSQL_SCHEMA = os.getenv("MYSQL_SCHEMA", "research")
+MYSQL_AUTH_PLUGIN = os.getenv("MYSQL_AUTH_PLUGIN", "mysql_native_password")
+PORT = int(os.getenv("PYTHON_PORT", 3001))
 
-pool: asyncmy.Pool | None = None
+ALLOWED_CONTENT_TYPE = "application/json"
+NOT_FOUND_ERR_BODY_RESPONSE = b"Not Found"
+HEADERS = [("content-type", ALLOWED_CONTENT_TYPE)]
+TEXT_HEADERS = [("content-type", "text/plain")]
+
+DB_CONNECTION_POOL: asyncmy.Pool | None = None
+TEST_AMOUNT = 0
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+app = Granian(
+    "main:main",
+    factory=True,  # use factory mode for initialization setup (see `initial_startup()`)
+    interface=Interfaces.RSGI,
+    loop=Loops.uvloop,
+    address="0.0.0.0",
+    port=PORT,
+)
+
 
 async def init_db():
     """Connect to database"""
-    global pool
-    if pool is None:
+    global DB_CONNECTION_POOL
+
+    if DB_CONNECTION_POOL is None:
         try:
-    
-            pool = await asyncmy.create_pool(
+
+            DB_CONNECTION_POOL = await asyncmy.create_pool(
                 host=MYSQL_HOST,
                 user=MYSQL_USER,
                 password=MYSQL_PASSWORD,
                 database=MYSQL_SCHEMA,
                 minsize=5,
-                maxsize=20
+                maxsize=20,
+
             )
-            print("MySQL pool connected")
+            logger.info("MySQL pool connected")
         except Exception as e:
-            print(f"Database connect error: {e}")
-            pool = None
+            logger.error(f"Database connect error: {e!r}")
+            DB_CONNECTION_POOL = None
+
 
 async def get_data():
     """Return data from database"""
-    if pool is None:
-        print("Database pool error.")
+    if DB_CONNECTION_POOL is None:
+        logger.error("Database pool error.")
         return None
 
-    async with pool.acquire() as connection:
+    async with DB_CONNECTION_POOL.acquire() as connection:
         async with connection.cursor() as cursor:
             await cursor.execute("SELECT * FROM test WHERE name = %s LIMIT 1", ('amount',))
             result = await cursor.fetchone()
+
     return result
+
 
 async def update_amount(amount):
     """Update amount in database"""
-    if pool is None:
-        print("Database pool error.")
+    if DB_CONNECTION_POOL is None:
+        logger.error("Database pool error.")
         return
 
-    async with pool.acquire() as connection:
+    async with DB_CONNECTION_POOL.acquire() as connection:
         async with connection.cursor() as cursor:
             await cursor.execute("UPDATE test SET code = %s WHERE name = %s", (amount, 'amount'))
             await connection.commit()
 
 
-amount = 1
-async def app(scope, receive, send):
+async def handle_request(scope: Scope, protocol: HTTPProtocol):
     """Start http-server app"""
-    global amount
+    global TEST_AMOUNT
 
-    if scope["type"] == "lifespan":
-        event = await receive()
-        if event["type"] == "lifespan.startup":
-            await init_db()
-            await update_amount(0)
-            await send({"type": "lifespan.startup.complete"})
+    TEST_AMOUNT += 1
+
+    path = scope.path
+    if path == "/endpoint_slow":
+        result = await get_data()
+        await update_amount(TEST_AMOUNT)
+        response_body = {"amount": TEST_AMOUNT, "result": result}
+
+    elif path == "/endpoint_fast":
+        response_body = {"amount": TEST_AMOUNT}
+
+    else:
+        protocol.response_bytes(404, HEADERS, NOT_FOUND_ERR_BODY_RESPONSE)
         return
 
-    if scope["type"] == "http":
-        path = scope['path']
-        amount += 1
+    protocol.response_bytes(200, HEADERS, orjson.dumps(response_body))
 
-        if path == "/endpoint_slow":
-            result = await get_data()
-            await update_amount(amount)
-            response_body = json.dumps({"amount": amount, "result": result})
 
-        elif path == "/endpoint_fast":
-            response_body = json.dumps({"amount": amount})
+async def handle_404(_: Scope, protocol: HTTPProtocol):
+    protocol.response_bytes(404, TEXT_HEADERS, NOT_FOUND_ERR_BODY_RESPONSE)
 
-        else:
-            response_body = json.dumps({"error": "Not Found"})
-            status = 404
-            headers = [(b"content-type", b"application/json")]
-            await send({"type": "http.response.start", "status": status, "headers": headers})
-            await send({"type": "http.response.body", "body": response_body.encode("utf-8")})
-            return
 
-        headers = [(b"content-type", b"application/json")]
-        await send({"type": "http.response.start", "status": 200, "headers": headers})
-        await send({"type": "http.response.body", "body": response_body.encode("utf-8")})
+async def initial_startup():
+    await init_db()
+    await update_amount(0)
+
+
+def controller(scope, proto):
+    if scope.proto == "http":
+        return handle_request(scope, proto)
+
+    return handle_404(scope, proto)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(initial_startup())
+
+    return controller
+
+
+if __name__ == "__main__":
+    try:
+        app.serve()
+    except KeyboardInterrupt:
+        pass
